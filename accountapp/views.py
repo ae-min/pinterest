f191ce52d0c64567cd99567278bd439cc146d27f
ae-min <papace@naver.com>
2021-12-04 16:48:19 +0900

    course 8 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,6 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
+    return render(request, 'base.html')
+    #템플릿을 가지고와서 내용을 채워넣는 형식. 요청이 들어오면 base.html 템플릿페이지 반환
+    #pinterest_settings_template DIR부분에 'DIRS': [os.path.join(BASE_DIR, 'templates')], 추가함
+
+    '''
     return HttpResponse('hello world!')
-    # pinterest_account -> account_urls -> views_hello_world -> hello world! 출력하는 구조
-    # http://127.0.0.1:8000/account/hello_world/ -> hello world! 출력
+    pinterest_account -> account_urls -> views_hello_world -> hello world! 출력하는 구조
+    http://127.0.0.1:8000/account/hello_world/ -> hello world! 출력
+    '''
+
