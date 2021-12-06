a4fe642740c469f2585c2eee6e98e676faad87af
ae-min <papace@naver.com>
2021-12-06 15:53:50 +0900

    course 16 commit - post, get

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,7 +4,17 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html', context={'text':'POST METHOD!!!'})
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
+
+    '''
+    요청을 받은 객체 안에서 찾은 메소드가 POST 메소드일 경우, 기존의 render 방식을 사용하는데, 
+    추가적으로 context라는 데이터 꾸러미 안에 텍스트라는 이름을 가졌고 내용은 POST METHOD!!!인 문장을 넣어서 리턴
+    -> hello_world.html의 {{ text }} 가 호출되면, context의 text를 찾고 POST METHOD!!! 출력
+    '''
 
     '''
     return render(request, 'base.html')
