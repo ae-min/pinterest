43f67cee1171fd52b9702ecc05380b49f67e504d
ae-min <papace@naver.com>
2021-12-12 21:26:22 +0900

    course 31 commit - Profileapp 구현시작

--- profileapp/forms.py
+++ profileapp/forms.py
@@ -7,3 +7,8 @@ class ProfileCreationForm(ModelForm):
         model = Profile
         fields = ['image', 'nickname', 'message']
 
+    '''
+    @user는 폼에 보여지지 않고 서버에서 만든 이유
+    클라이언트에서 이미지, 닉네임, 메시지의 주인이 누구인지 user를 통해 보여주게 되면, 
+    그 정보들을 활용해서 다른 사람의 프로필을 만들 수도 있으므로, 악용 방지를 위해 user는 서버내에서 구현하기로 함.(views.py)
+    '''
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/create.html'
+
+    def form_valid(self, form):
+    #여기의 form은 forms.py의 class ProfileCreationForm(ModelForm):의 전체 데이터
+    #models.py의 Profile에는 user, image, ninkname, message가 있지만, forms.py에는 user를 제외한 3가지 데이터만 있는 상태
+        temp_profile = form.save(commit=False)
+        #실제 데이터베이스에 저장하지 않고, 임시 대기 중인 데이터이므로 커밋하지 않게 지정
+        #forms.py에 있는 3가지 정보는 있지만, 거기 정의되지 않은 user 데이터는 아직 없는 상태
+        temp_profile.user = self.request.user
+        #temp_profile의 user라는 데이터를,  self해서 리퀘스트를 보낸 그 당사자 유저로 정해줌
+        temp_profile.save() #그 후, 최종 저장
+        return super().form_valid(form) #나머지는 조상인 class ProfileCreateView(CreateView):의 결과를 리턴
